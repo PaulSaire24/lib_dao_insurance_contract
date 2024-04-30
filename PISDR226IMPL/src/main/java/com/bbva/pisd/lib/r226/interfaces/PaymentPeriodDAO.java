8f1957279345d7817f6f6f2e68617c6a6af49edb
Isaac Duberli Mogrovejo Tucto <isaac.mogrovejo.contractor@bbva.com>
2024-04-30 14:16:33 -0500

    PARSRBVD-2671: se quitaron metodos que no son necesarios para la version actual del componente

--- PISDR226IMPL/src/main/java/com/bbva/pisd/lib/r226/interfaces/PaymentPeriodDAO.java
+++
@@ -1,9 +0,0 @@
-package com.bbva.pisd.lib.r226.interfaces;
-
-import com.bbva.pisd.dto.insurancedao.entities.PaymentPeriodEntity;
-
-public interface PaymentPeriodDAO {
-
-    PaymentPeriodEntity findPaymentPeriodByType(String paymentFrequencyType);
-
-}
