85fe98744aed938cf65dcde892d7186154daf246
Isaac Duberli Mogrovejo Tucto <isaac.mogrovejo.contractor@bbva.com>
2024-06-26 17:50:44 -0500

    PARSRBVD-4067: homologacion metodo job de fraude

--- PISDR226IMPL/src/main/java/com/bbva/pisd/lib/r226/transfor/map/ContractTransformMap.java
+++ PISDR226IMPL/src/main/java/com/bbva/pisd/lib/r226/transfor/map/ContractTransformMap.java
@@ -1,5 +1,6 @@
 package com.bbva.pisd.lib.r226.transfor.map;
 
+import com.bbva.pisd.dto.contract.search.CertifyBankCriteria;
 import com.bbva.pisd.dto.insurancedao.constants.PISDColumn;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -58,5 +59,18 @@ public class ContractTransformMap {
         return arguments;
     }
 
+    public static Map<String, Object> certifyBankCriteriaTransformMap(CertifyBankCriteria certifyBankCriteria){
+        LOGGER.info("[***] ContractTransformMap certifyBankCriteriaTransformMap receiptSearchCriteriaDTO - {} ",certifyBankCriteria);
 
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put(PISDColumn.Contract.FIELD_INSURANCE_CONTRACT_ENTITY_ID, certifyBankCriteria.getInsuranceContractEntityId() );
+        parameters.put(PISDColumn.Contract.FIELD_INSURANCE_CONTRACT_BRANCH_ID, certifyBankCriteria.getInsuranceContractBranchId() );
+        parameters.put(PISDColumn.Contract.FIELD_CONTRACT_FIRST_VERFN_DIGIT_ID, certifyBankCriteria.getContractFirstVerfnDigitId());
+        parameters.put(PISDColumn.Contract.FIELD_CONTRACT_SECOND_VERFN_DIGIT_ID, certifyBankCriteria.getContractSecondVerfnDigitId());
+        parameters.put(PISDColumn.Contract.FIELD_INSRC_CONTRACT_INT_ACCOUNT_ID, certifyBankCriteria.getInsrcContractIntAccountId());
+
+        LOGGER.info("[***] ContractTransformMap certifyBankCriteriaTransformMap parameters - {} ",parameters);
+
+        return parameters;
+    }
 }
